6bcfb022b24b37735ab04c9f7358bbed33466b6c
Ashokkumar <ashokkumarash96@gmail.com>
2023-02-26 13:55:17 +0530

    update

--- src/data.js
+++ src/data.js
@@ -37,7 +37,7 @@ const questions = [
   },
   {
     id: 7,
-    question: "What is the difference between state and props?",
+    question: "What is the d/b state and props?",
     answer:
       "Both props and state are plain JavaScript objects. While both of them hold information that influences the output of render, they are different in their functionality with respect to component. Props get passed to the component similar to function parameters whereas state is managed within the component similar to variables declared within a function.",
   },
